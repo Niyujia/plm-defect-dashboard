00fb303866296f5dcaa9a40eefb6a09d08b53a6e
niyujia <niyujia@users.noreply.github.com>
2026-06-02 18:24:51 +0800

    Update dashboard 20260602

--- start_server.bat
+++ start_server.bat
@@ -1,9 +1,3 @@
 @echo off
-REM PLM缺陷看板 - 内网HTTP服务器启动脚本
-REM 启动后可通过 http://10.199.183.123:8080 访问
-
-echo Starting PLM Dashboard Server...
-echo URL: http://10.199.183.123:8080
-echo Press Ctrl+C to stop
-
-"C:\Users\niyujia\.workbuddy\binaries\python\envs\default\Scripts\python.exe" -m http.server 8080 --directory "C:\Users\niyujia\plm-dashboard-web" --bind 0.0.0.0
+cd /d C:\Users\niyujia\plm-dashboard-web
+start "" /MIN C:\Users\niyujia\.workbuddy\binaries\python\versions\3.13.12\python.exe -m http.server 8080
